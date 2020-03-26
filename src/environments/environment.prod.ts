250f749bc89b970184fb13344467ec694ac4b139
Ashish Rana <aashishrana269@gmail.com>
2020-03-26 12:14:26 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig :{
-    apiKey: "AIzaSyD9FJlfkEKQr-QCxZxlKTDl6zvAkGNNI80",
+    apiKey: "",
     authDomain: "crudapp08.firebaseapp.com",
-    databaseURL: "https://crudapp08.firebaseio.com",
-    projectId: "crudapp08",
-    storageBucket: "crudapp08.appspot.com",
-    messagingSenderId: "523754970869",
-    appId: "1:523754970869:web:de919f4914edbfe38fefbc",
-    measurementId: "G-DB0EXZ110Z"
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
