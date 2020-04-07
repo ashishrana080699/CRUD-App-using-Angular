5b9f224935e548beb6700ed48807bfc04c37089d
Ashish Rana <aashishrana269@gmail.com>
2020-04-07 21:20:43 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {environment} from '../environments/environment';
 
+
 @NgModule({
   declarations: [
     AppComponent,
