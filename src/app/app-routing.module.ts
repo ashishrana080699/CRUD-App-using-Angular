55c041770e5fb13ae14ee900b5482a83d86bdb06
Ashish Rana <aashishrana269@gmail.com>
2020-04-23 01:04:58 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomerComponent } from './customer/customer.component';
-
+ 
  
 const routes: Routes = [{
     path: '', redirectTo: 'customer',pathMatch: 'full'
